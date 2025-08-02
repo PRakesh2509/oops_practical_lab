501afd10721817581ccb20a3fa157824bcea5bd5
PRakesh2509 <rakeshpavuluri999@gmail.com>
2025-08-02 22:36:27 +0530

    Add files via upload

---
+++ SubstringExample.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class SubstringExample {
+	public static void main(String[] args) {
+        String s1 = "JavaProgramming";
+        String sub = s1.substring(4, 11); // "Program"
+        System.out.println("Substring: " + sub);
+    }
+}
